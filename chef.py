77fc01d631a68aef704b7bb7050f8251fbab005f
hlkhala022 <hlkhala022@student.wethinkcode.co.za>
2024-03-15 14:02:52 +0200

    AI now listening to my voicee instructions

--- chef.py
+++ chef.py
@@ -1,10 +1,27 @@
 import google.generativeai as genai
 import pyttsx3
+import speech_recognition as sr
+
 
 def speak(text):
     engine = pyttsx3.init()
     engine.say(text)
     engine.runAndWait()
+
+def get_voice_input():
+    recognizer = sr.Recognizer()
+    with sr.Microphone() as source:
+        speak("Please speak your ingredients")
+        audio = recognizer.listen(source)
+        try:
+            return recognizer.recognize_google(audio)
+        except sr.UnknownValueError:
+            speak("Sorry, I didn't hear you.")
+            return None
+        except sr.RequestError:
+            speak("oh no! I had a problem connecting to the speech services.")
+            return None
+
 
 def generate_recipe(ingredients, api_key):
     genai.configure(api_key=api_key)
@@ -16,14 +33,17 @@ def generate_recipe(ingredients, api_key):
 
 speak("Please enter your ingredients for your recipe. Seperate your ingredients with a comma")
 
-user_input = input("enter your ingredients for your recipe (Seperate the ingredients with a comma (,) ): ")
+user_input = get_voice_input()
+if user_input:
+    ingredients = [ingredient.strip() for ingredient in user_input.split(',')]
 
-ingredients = [ingredient.strip() for ingredient in user_input.split(',')]
+    api_key = "AIzaSyCioqqaiWEacgniO7lXyjmx_YFgFUvbvUA"
 
-api_key = "AIzaSyCioqqaiWEacgniO7lXyjmx_YFgFUvbvUA"
-
-generated_recipe = generate_recipe(ingredients, api_key)
+    generated_recipe = generate_recipe(ingredients, api_key)
 
 
-print(generated_recipe)
-speak(generated_recipe)
+    print(generated_recipe)
+    speak(generated_recipe)
+
+else:
+    speak("No ingredients were provided.")
